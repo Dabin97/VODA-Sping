113b6b523953224adecc6d871080487ccd437cec
안가영 <1036283@gmail.com>
2023-03-20 16:51:31 +0900

    GY

--- VodaSpringBoard/src/main/java/com/voda/MainController.java
+++ VodaSpringBoard/src/main/java/com/voda/MainController.java
@@ -1,8 +1,13 @@
 package com.voda;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -41,6 +46,25 @@ public class MainController {
 	@RequestMapping("/main")
 	public String main() {
 		return "main";
+	}
+	
+	
+	@RequestMapping("/review/like/{rno}")
+	public ResponseEntity<String> reviewLike(@PathVariable(name = "rno") int rno, 
+			HttpSession session){
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		MemberDTO dto = (MemberDTO) session.getAttribute("dto");
+		
+		int result = boardService.insertReviewLike(rno,dto.getId());
+		
+		if(result == 0)
+			map.put("msg", "좋아요를 취소하셨습니다.");
+		else
+			map.put("msg", "좋아요를 하셨습니다.");
+		
+		map.put("blike",boardService.selectReviewLike(rno));
+		
+		return new ResponseEntity(map,HttpStatus.OK);
 	}
 	
 
