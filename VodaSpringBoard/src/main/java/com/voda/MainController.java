db9fbcbbda40993a7db66c067ea1a0739e8e5672
jjwu99 <99jjwu@gmail.com>
2023-03-20 15:23:03 +0900

    1 commit

--- VodaSpringBoard/src/main/java/com/voda/MainController.java
+++ VodaSpringBoard/src/main/java/com/voda/MainController.java
@@ -1,7 +1,13 @@
 package com.voda;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.voda.dto.MemberDTO;
+
+
 
 @Controller
 public class MainController {
@@ -10,10 +16,22 @@ public class MainController {
 		return "index";
 	}
 	
+	
 	@RequestMapping("/register")
 	public String registerView() {
 		return "register";
 	}
+
+	@RequestMapping("/login")
+	public String login(String id, String passwd, HttpSession session) {
+		MemberDTO dto = memberService.login(id, passwd);
+		session.setAttribute("dto", dto);
+		return "redirect:/main";
+	}
 	
-	
+	@RequestMapping("/member/edit")
+	public String memberEdit() {
+		return "admin_member_edit";
+	}
+
 }
---
+++ VodaSpringBoard/src/main/java/com/voda/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.voda.mapper;
+
+import java.util.HashMap;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.voda.dto.MemberDTO;
+
+@Mapper
+public interface MemberMapper {
+
+	MemberDTO login(HashMap<String, Object> map);
+
+}
+
