861f226bfe7602ba0a4a2ec76e7830c9e0ad286f
Dabin97 <127208280+Dabin97@users.noreply.github.com>
2023-04-18 17:40:36 +0900

    Update VodaSpringBoardApplication.java

--- VodaSpringBoard/src/main/java/com/voda/VodaSpringBoardApplication.java
+++ VodaSpringBoard/src/main/java/com/voda/VodaSpringBoardApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VodaSpringBoardApplication {
-
 	public static void main(String[] args) {  
 		SpringApplication.run(VodaSpringBoardApplication.class, args); 
 	}  
